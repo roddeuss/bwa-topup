174d56ff833832d8047a811d6a6d02e37e1f6638
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-09-17 11:36:22 +0700

    update

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -1,13 +1,15 @@
 import cx from "classnames";
 import Image from "next/image"
+import Link from "next/link";
 
 interface MenuItemProps {
     title: string;
     icon: 'ic-menu-overview' | 'ic-menu-card' | 'ic-menu-logout' | 'ic-menu-messages' | 'ic-menu-setting' | 'ic-menu-transactions' | 'ic-menu-rewards'
     active?: boolean;
+    href: string
 }
-export default function MenuItem(props: Partial <MenuItemProps>) {
-    const{title, icon, active} = props;
+export default function MenuItem (props: Partial <MenuItemProps>) {
+    const{title, icon, active, href} = props;
     const classItem  = cx ({
         'item' : true,
         'mb-30' : true,
@@ -19,7 +21,9 @@ export default function MenuItem(props: Partial <MenuItemProps>) {
                         <Image src={`/icon/${icon}.svg`} width={25} height={25} alt="overview" />
                         </div>
                         <p className="item-title m-0">
-                            <a href="" className="text-lg text-decoration-none">{title}</a>
+                            <Link href={href || '#'} >
+                            <a className="text-lg text-decoration-none">{title}</a>
+                            </Link>
                         </p>
                     </div>
   )
--- components/organisms/Sidebar/index.tsx
+++ components/organisms/Sidebar/index.tsx
@@ -2,19 +2,23 @@ import Footer from "../Footer"
 import MenuItem from "./MenuItem"
 import Profile from "./Profile"
 
-export default function Sidebar() {
+interface SidebarProps {
+    activeMenu:  'overview' | 'transactions' | 'settings'
+}
+export default function Sidebar(props: SidebarProps) { 
+    const {activeMenu} = props;
   return (
     <section className="sidebar">
             <div className="content pt-50 pb-30 ps-30">
                 <Profile />
                 <div className="menus">
-                <MenuItem  icon="ic-menu-overview" title="Overview" active/>
-                <MenuItem  icon="ic-menu-transactions" title="Transactions" />
-                <MenuItem  icon="ic-menu-messages" title="Messages" />
-                <MenuItem  icon="ic-menu-card" title="Card" />
-                <MenuItem  icon="ic-menu-rewards" title="Rewards" />
-                <MenuItem  icon="ic-menu-setting" title="Settings" />
-                <MenuItem  icon="ic-menu-logout" title="Log Out" />
+                <MenuItem  icon="ic-menu-overview" title="Overview" active={activeMenu === 'overview'} href="/"/>
+                <MenuItem  icon="ic-menu-transactions" title="Transactions" active={activeMenu === 'transactions'} href="/member/transactions"/>
+                <MenuItem  icon="ic-menu-messages" title="Messages" href="/member" />
+                <MenuItem  icon="ic-menu-card" title="Card" href="/member" />
+                <MenuItem  icon="ic-menu-rewards" title="Rewards" href="/member" />
+                <MenuItem  icon="ic-menu-setting" title="Settings" active={activeMenu === 'settings'} href="/member/edit-profile"/>
+                <MenuItem  icon="ic-menu-logout" title="Log Out" href="/sign-in" />
                 </div>
                 <Footer />
             </div>
